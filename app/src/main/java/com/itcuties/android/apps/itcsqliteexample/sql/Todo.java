cb6fbf2a2a8c581103de1f33eee2789742ff56e7
ashley.stalvig <astalvig@css.edu>
2016-04-26 18:23:05 -0500

    Initial commit

---
+++ app/src/main/java/com/itcuties/android/apps/itcsqliteexample/sql/Todo.java
@@ -0,0 +1,26 @@
+package com.itcuties.android.apps.itcsqliteexample.sql;
+
+public class Todo {
+
+	private int id;
+	private String text;
+    private int priority;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Integer getPriority() { return priority; }
+    public void setPriority(Integer priority) {this.priority = priority;}
+	
+}
